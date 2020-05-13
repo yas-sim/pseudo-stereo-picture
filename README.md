a43e046fc09b9adf8eb8a02cbca658b2a762a9e6
Yasunori Shimura <63567854+yas-sim@users.noreply.github.com>
2020-05-14 00:16:42 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,82 @@
+# Pseudo Stereo Picture Generation Demo
+This program generates a set of pseudo stereo pictures from an input image.using  the [Intel(r) Distribution of OpenVINO(tm) toolkit](https://software.intel.com/en-us/openvino-toolkit).  
+The demo program uses a DL model, `midasnet` to generate a depth map from a picture , shift the pixcels horizontally according to the depth of the pixels, then compensate the lacking pixcels caused by the shifted pixels with an image inpainting DL model `gmcnn`.  
+
+このデモプログラムは[Intel(r) Distribution of OpenVINO(tm) toolkit](https://software.intel.com/en-us/openvino-toolkit)を使用し、１枚の絵から疑似ステレオ画像を作成するものです。  
+プログラムは入力画像から`midasnet` DLモデルを使用して深度マップを作成し、ピクセル深度に従って各ピクセルを横方向にシフトし、ピクセルシフトによりできた欠損部分を`gmcnn`画像修復DLモデルで補うことで、疑似ステレオ画像（っぽい）画像を生成しています。
+
+
+### Generated Pseudo Stereo Images
+![stereo](./resources/pseudo-stereo.gif)
+
+### Depth Image Generated From the Input Picture (This demo won't display this image)
+![depth](./resources/disp.png)
+
+### Required DL Models to Run This Demo
+
+The demo expects the following models in the Intermediate Representation (IR) format:
+
+  * `midasnet`
+  * `gmcnn-places2-tf`
+
+You can download this model from OpenVINO [Open Model Zoo](https://github.com/opencv/open_model_zoo).
+In the `models.lst` is the list of appropriate models for this demo that can be obtained via `Model downloader`.
+Please see more information about `Model downloader` [here](../../../tools/downloader/README.md).
+
+## How to Run
+
+
+### 0. Prerequisites
+- **OpenVINO 2020.2**
+  - If you haven't installed it, go to the OpenVINO web page and follow the [*Get Started*](https://software.intel.com/en-us/openvino-toolkit/documentation/get-started) guide to do it.  
+
+
+### 1. Install dependencies  
+The demo depends on:
+- `opencv-python`
+- `numpy`
+
+To install all the required Python modules you can use:
+
+``` sh
+(Linux) pip3 install -r requirements.txt
+(Win10) pip install -r requirements.txt
+```
+
+### 2. Download DL models from OMZ
+Use `Model Downloader` to download the required models and convert the downloaded model into OpenVINO IR models with `Model Converter`.  
+The source model of the `midasnet` is in PyTorch format. Please make sure you have installed prerequisites for `pytorch_to_onnx.py` converter tool.  
+
+``` sh
+(Linux) 
+  # Install prerequisites for pytorch_to_onnx.py. This is required only for the 1st time
+  python3 -m pip install -r $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/requirements-pytorch.in
+  python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/downloader.py --list models.lst
+  python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/converter.py --list models.lst
+       
+(Win10) 
+  # Install prerequisites for pytorch_to_onnx.py. This is required only for the 1st time
+  python3 -m pip install -r "%INTEL_OPENVINO_DIR%\deployment_tools\tools\model_downloader\requirements-pytorch.in"
+  python "%INTEL_OPENVINO_DIR%\deployment_tools\tools\model_downloader\downloader.py" --list models.lst
+  python "%INTEL_OPENVINO_DIR%\deployment_tools\tools\model_downloader\converter.py" --list models.lst
+```
+
+### 3. Run the demo app
+
+``` sh
+(Linux) python3 pseudo-stereo-picture.py <input_image_file>
+(Win10) python pseudo-stereo-picture.py <input_image_file>
+```
+
+## Demo Output  
+The application draws the results on the screen.
+
+## Tested Environment  
+- Windows 10 x64 1909 and Ubuntu 18.04 LTS  
+- Intel(r) Distribution of OpenVINO(tm) toolkit 2020.2  
+- Python 3.6.5 x64  
+
+## See Also  
+* [Using Open Model Zoo demos](../../README.md)  
+* [Model Optimizer](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html)  
+* [Model Downloader](../../../tools/downloader/README.md)  
---
+++ pseudo-stereo-picture.py
@@ -0,0 +1,94 @@
+import sys
+
+import numpy as np 
+import cv2
+
+from openvino.inference_engine import IECore
+
+def main():
+    if len(sys.argv)<2:
+        print('please specify an input file', file=sys.stderr)
+        sys.exit(-1)
+    
+    input_img = cv2.imread(sys.argv[1])
+
+    ie = IECore()
+
+    model = 'midasnet'  # image [1, 3, 384, 384] inverse_depth [1, 384, 384]
+    model = './public/'+model+'/FP16/'+model
+    net_depth = ie.read_network(model+'.xml', model+'.bin')
+    inBlobName_depth = list(net_depth.inputs.keys())[0]
+    outBlobName_depth = list(net_depth.outputs.keys())[0]
+    inBlobShape_depth = net_depth.inputs[inBlobName_depth].shape
+    outBlobShape_depth = net_depth.outputs[outBlobName_depth].shape
+    #print(inBlobName_depth, inBlobShape_depth, outBlobName_depth, outBlobShape_depth)
+    execNet_depth = ie.load_network(net_depth, 'CPU')
+
+    model = 'gmcnn-places2-tf'  # Placeholder [1, 3, 512, 680] Placeholder_1 [1, 1, 512, 680] Minimum [1, 3, 512, 680]
+    model = './public/'+model+'/FP16/'+model
+    net_inpaint = ie.read_network(model+'.xml', model+'.bin')
+    inBlobName_inpaint0 = list(net_inpaint.inputs.keys())[0]
+    inBlobName_inpaint1 = list(net_inpaint.inputs.keys())[1]
+    outBlobName_inpaint = list(net_inpaint.outputs.keys())[0]
+    inBlobShape_inpaint0 = net_inpaint.inputs[inBlobName_inpaint0].shape
+    inBlobShape_inpaint1 = net_inpaint.inputs[inBlobName_inpaint1].shape
+    outBlobShape_inpaint = net_inpaint.outputs[outBlobName_inpaint].shape
+    #print(inBlobName_inpaint0, inBlobShape_inpaint0, inBlobName_inpaint1, inBlobShape_inpaint1, outBlobName_inpaint, outBlobShape_inpaint)
+    execNet_inpaint = ie.load_network(net_inpaint, 'CPU')
+
+    # Generate depth image
+    img = cv2.resize(input_img, (inBlobShape_depth[3], inBlobShape_depth[2]))
+    img = img.transpose((2,0,1))
+    img = img.reshape(inBlobShape_depth)
+    res_depth = execNet_depth.infer(inputs={inBlobName_depth: img})
+    invDepth = res_depth[outBlobName_depth].transpose((1,2,0))
+    resizedInvDepth = cv2.resize(invDepth, (inBlobShape_inpaint0[3], inBlobShape_inpaint0[2]))
+    depth_min = resizedInvDepth.min()
+    depth_max = resizedInvDepth.max()
+    print('depth range:', depth_min, depth_max)
+
+    resizedImg = cv2.resize(input_img, (inBlobShape_inpaint0[3], inBlobShape_inpaint0[2]))
+
+    imageStack = []
+    shiftRange = 20
+    print('Shift range:', -shiftRange, shiftRange)
+    for shift in range(-shiftRange, shiftRange+1, 2):
+        print('Shift:', shift)
+        img = np.full(inBlobShape_inpaint0, 255, dtype=np.uint8)
+        msk = np.full(inBlobShape_inpaint1, -1.0, dtype=np.float32)
+
+        for y in range(inBlobShape_inpaint0[2]):
+            for x in range(inBlobShape_inpaint0[3]):
+                pixel = resizedImg[y,x,:]
+                depth = resizedInvDepth[y,x]
+                xx = int(x + (shift*((depth-depth_min)/depth_max)))  # Shift pixel position by depth
+                # Check depth (Z buffer) and X coordinate before fill pixel
+                if msk[0,0,y,x]<depth and xx>=0 and xx<inBlobShape_inpaint0[3]:
+                    img[0,:,y,xx] = pixel
+                    msk[0,0,y,xx] = depth
+
+        msk = np.where(msk==-1., 1.0, 0.0)   # non filled pixels = lacking(=1.0)
+
+        res_inpaint = execNet_inpaint.infer(inputs={inBlobName_inpaint0: img, inBlobName_inpaint1: msk})
+
+        out = np.transpose(res_inpaint[outBlobName_inpaint], (0, 2, 3, 1)).astype(np.uint8)
+        out = cv2.cvtColor(out[0], cv2.COLOR_RGB2BGR)
+
+        imageStack.append(out)    # Append the new picture to stack
+        cv2.imshow('Result', out)
+
+        key = cv2.waitKey(1)
+        if key==27:
+            break
+
+    # Show results
+    while True:
+        for image in imageStack:
+            cv2.imshow('Result', image)
+            if cv2.waitKey(30)==27: return
+        for image in imageStack[::-1]:
+            cv2.imshow('Result', image)
+            if cv2.waitKey(30)==27: return
+
+if __name__ == '__main__':
+    main()
